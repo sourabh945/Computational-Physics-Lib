181f9201d3497e05d0bd6687502fc6729d4c164d
Sourabh <125686581+sourabh945@users.noreply.github.com>
2023-08-21 23:13:17 +0530

    modified:   Monticarlo method/main.f95 	modified:   a.out 	modified:   allpoints.dat 	modified:   innerpoints.dat

--- Monticarlo method/main.f95
+++ Monticarlo method/main.f95
@@ -1,22 +1,35 @@
 program monticarlo
 
+    implicit none
+
     real x ,y ,m ,n ,radius ,area ,length ,distance ,error ,area2
     real, parameter :: pi = 3.141592653589793
     integer all , inner ,i
     logical f1,f2
 
+! getting the input
+
     print *, "Enter the value of the radius of the circle :: "
     read(*,*)radius
-    print *, "Enter the number of the points maps :: "
+    print *, "Enter the number of the points uses :: "
     read(*,*)all
 
-    if ((radius <= 0).and.(all <= 0)) then
-        print *, "The Program is not working with these value of the radius and points maps"
+! check the value of the radius and points fit for program
+
+    if (radius <= 0)then
+        print *, "The Program is not working with these value of the radius"
         call Exit(1)
     endif
 
-    inquire(file="allpoints.dat",exist=f1)
-    inquire(file="innerpoints.dat",exist=f2)
+    if (all <= 0)then
+        print *, "The Program is not working with these value of the points maps"
+        call Exit(1)
+    endif
+
+! check exiting file and open it and if file doesn't exit then create the files
+
+    inquire(file="allpoints.dat",exist=f1) ! for checking the existance of the file
+    inquire(file="innerpoints.dat",exist=f2) 
     if (f1) then
         open(1,file="allpoints.dat",status="replace")
     else
@@ -28,12 +41,15 @@ program monticarlo
         open(2,file="innerpoints.dat",status="new",action="write")
     endif
 
+! defining the square space use for program
     length = 2*radius
+
     inner = 0
 
+! loop for generating the random points and check how many are inside the circle
     do i = 1,all
         call random_number(m)
-        x = length*m
+        x = length*m ! this because random_number generate only value between 0 and 1
         call random_number(n)
         y = length*n
         write(1,*) x,y
@@ -44,8 +60,10 @@ program monticarlo
         endif
     enddo
     
-    
+!  printing the all result and calculating the error
+
     area = ((real(inner))/real(all))*(length**2)
+
     print *, "The area of the circle by Monti Carlo method is :: ",area
     print *, "Inner points :: ",inner
     area2 = pi*(radius**2)
@@ -53,7 +71,7 @@ program monticarlo
     print *, "The area by simple formula is :: ", area2
     print *, "The percentage error :: ", error
 
-    close(1)
+    close(1) ! Closing the files
     close(2)
 
     stop
