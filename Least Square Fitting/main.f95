196f6bac8227752369e285dc69e7788a962e5ca6
Sourabh <sourabhsheokand945@gmail.com>
2023-09-03 00:01:55 +0530

    new file:   Least Square Fitting/lst.out 	deleted:    Least Square Fitting/main 	modified:   Least Square Fitting/main.f95

--- Least Square Fitting/main.f95
+++ Least Square Fitting/main.f95
@@ -1,18 +1,26 @@
 program lsf
+
     implicit none
+
+    ! declaration of the variables 
 
     real , dimension(:) , allocatable :: x,y
     real sumx , sumy , sumxx , sumxy , c , m
     integer i ,n
-    logical f1
+    
+    ! Inzillizating the value of the variable
 
     sumx = 0 
     sumy = 0 
     sumxx = 0
     sumxy = 0
 
+    ! getting number  of the points given
+
     print *, "Enter the number of the entry required :: "
     read *, n
+
+    ! allocate the array for store the value of the x and y coordinates and getting from the user
 
     allocate(x(n))
     allocate(y(n))
@@ -21,6 +29,8 @@ program lsf
     print *, "x      y"
     read *, (x(i),y(i),i=1,n)
 
+    ! find the required terms with summations over the input
+
     do i = 1,n
         sumx = sumx + x(i)
         sumy = sumy + y(i)
@@ -28,8 +38,20 @@ program lsf
         sumxy = sumxy + x(i)*y(i)
     enddo
 
-    deallocate(x(n))
-    deallocate(y(n))
+    ! deallocate the arrays to free the memory
+
+    deallocate(x)
+    deallocate(y)
+
+    ! calculating the intersection and the slope of the line
+
+    c = (sumxx*sumy - sumxy*sumx)/(n*(sumxx)-(sumx)**2)
+    m = (n*sumxy - sumx*sumy)/(n*sumxx - (sumx)**2)
+
+    ! giving the output 
+
+    print *, "The value of slope is ",m," and the value of y axis intersection is ",c
+    print *, "The equation of the line is :: y = ",m,"x +",c
 
     stop 
 
