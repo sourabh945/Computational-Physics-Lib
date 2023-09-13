ab3857a0db58d76eb58be4b58c8520df02fe2efd
Sourabh <sourabhsheokand945@gmail.com>
2023-09-13 23:22:37 +0530

    modified:   Frequency response of LCR/main.f95

--- Frequency response of LCR/main.f95
+++ Frequency response of LCR/main.f95
@@ -2,7 +2,10 @@ program main
 
     implicit none
 
-    real :: resistance , inductance , capicatance , impedence , freq_lower , freq_upper , step_num
+    real :: resistance , inductance , capicatance , impedence , freq_lower , freq_upper , step 
+    integer :: step_num , i
+    real , parameter :: pi = 3.14159265
+    logical :: f1
 
     print *, "Enter the value of resistance : "
     read *, resistance
@@ -15,4 +18,35 @@ program main
     read *, freq_lower
     print *, "Enter the upper limit of frequency : "
     read *, freq_upper
-    print *, "Enter the step taken to t
+    print *, "Enter the step taken to from lower frequency to upper frequency : "
+    read *, step_num
+
+    step = (freq_upper - freq_lower)/(real(step_num))
+
+    !check exiting file and open it and if file doesn't exit then create the files
+
+    inquire(file="freq_response.dat",exist=f1) ! for checking the existance of the file
+    if (f1) then
+        open(1,file="freq_response.dat",status="replace")
+    else
+        open(1,file="freq_response.dat",status="new",action="write")
+    endif
+
+    do i = 0,step_num
+        impedence = sqrt(resistance**2 + (2*pi*(freq_lower + i*step)*inductance - 1/(2*pi*(freq_lower + i*step)*capicatance))**2)
+        write(1,*)freq_lower+i*step,impedence
+    
+    enddo
+
+    close(1)
+
+    call execute_command_line("chmod +x ./plot.sh")
+
+    call execute_command_line("./plot.sh")
+
+    print *, "Frequency response is show in graph"
+
+    stop
+
+end program 
+    
