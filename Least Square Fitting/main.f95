1ee457e54d0fb79201f2eeffe93aa4de4b7836c5
Sourabh <sourabhsheokand945@gmail.com>
2023-09-18 18:18:15 +0530

    new file:   Least Square Fitting/Output.png 	new file:   Least Square Fitting/allpoints.dat 	new file:   Least Square Fitting/fittedpoints.dat 	new file:   Least Square Fitting/lsf.out 	deleted:    Least Square Fitting/lst.out 	modified:   Least Square Fitting/main.f95 	modified:   Least Square Fitting/output.txt

--- Least Square Fitting/main.f95
+++ Least Square Fitting/main.f95
@@ -4,9 +4,10 @@ program lsf
 
     ! declaration of the variables 
 
-    real , dimension(:) , allocatable :: x,y
-    real sumx , sumy , sumxx , sumxy , c , m
-    integer i ,n
+    real , dimension(:) , allocatable :: x,y,f
+    real :: sumx , sumy , sumxx , sumxy , c , m
+    integer ::  i ,n
+    logical :: f1 , f2
     
     ! Inzillizating the value of the variable
 
@@ -14,6 +15,21 @@ program lsf
     sumy = 0 
     sumxx = 0
     sumxy = 0
+
+    ! check exiting file and open it and if file doesn't exit then create the files
+
+    inquire(file="allpoints.dat",exist=f1) ! for checking the existance of the file
+    inquire(file="fittedpoints.dat",exist=f2) 
+    if (f1) then
+        open(1,file="allpoints.dat",status="replace")
+    else
+        open(1,file="allpoints.dat",status="new",action="write")
+    endif
+    if (f1) then
+        open(2,file="fittedpoints.dat",status="replace")
+    else
+        open(2,file="fittedpoints.dat",status="new",action="write")
+    endif
 
     ! getting number  of the points given
 
@@ -32,6 +48,7 @@ program lsf
     ! find the required terms with summations over the input
 
     do i = 1,n
+        write(1,*)x(i),y(i)
         sumx = sumx + x(i)
         sumy = sumy + y(i)
         sumxx = sumxx + (x(i))**2
@@ -40,18 +57,36 @@ program lsf
 
     ! deallocate the arrays to free the memory
 
-    deallocate(x)
     deallocate(y)
 
     ! calculating the intersection and the slope of the line
 
+
     c = (sumxx*sumy - sumxy*sumx)/(n*(sumxx)-(sumx)**2)
     m = (n*sumxy - sumx*sumy)/(n*sumxx - (sumx)**2)
+
+    ! storing the fitted points
+
+    allocate(f(n)) ! allocating the vector for fitting points
 
     ! giving the output 
 
     print *, "The value of slope is ",m," and the value of y axis intersection is ",c
     print *, "The equation of the line is :: y = ",m,"x +",c
+    print *, "       x            fitted points "
+
+    do i = 1,n ! calculated the fitting points
+        f(i) = m*x(i) + c
+        write(2,*)x(i),f(i)
+        print *, x(i),f(i)
+    enddo
+
+    deallocate(f)
+    deallocate(x)
+
+    close(1)
+    close(2)
+
 
     stop 
 
