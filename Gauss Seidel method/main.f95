baa4c2f4f1b4f0b091b4a5f4aeceec0718ef416c
Sourabh <sourabhsheokand945@gmail.com>
2023-09-24 07:53:41 +0530

    modified:   Gauss Seidel method/main 	modified:   Gauss Seidel method/main.f95 	deleted:    Gauss elimination method/main.f95 	deleted:    Gauss elimination method/output.txt 	deleted:    Gauss elimination method/sollineq.out 	deleted:    Lagrange Interpolation/interpolation.out 	deleted:    Lagrange Interpolation/main.f95 	deleted:    Lagrange Interpolation/output.txt 	deleted:    Least Square Fitting/lst.out 	deleted:    Least Square Fitting/main.f95 	deleted:    Least Square Fitting/output.txt 	deleted:    Monticarlo method/Area of circle/Output.txt 	deleted:    Monticarlo method/Area of circle/allpoints.dat 	deleted:    Monticarlo method/Area of circle/innerpoints.dat 	deleted:    Monticarlo method/Area of circle/main.f95 	deleted:    Monticarlo method/Area of circle/monticarlo.out 	deleted:    Monticarlo method/Area of circle/result for radius 5 and 10000 points.png 	deleted:    Monticarlo method/Simulation of Radioactive decay/Documentation for theory.pdf 	deleted:    Monticarlo method/Simulation of Radioactive decay/decay.dat 	deleted:    Monticarlo method/Simulation of Radioactive decay/main.f95 	deleted:    Monticarlo method/Simulation of Radioactive decay/output.png 	deleted:    Monticarlo method/Simulation of Radioactive decay/output.txt 	deleted:    Monticarlo method/Simulation of Radioactive decay/simulation.out 	deleted:    Povit Condensation/determinate of matrix/determinant.out 	deleted:    Povit Condensation/determinate of matrix/main.f95 	deleted:    Povit Condensation/determinate of matrix/output.txt 	deleted:    Povit Condensation/inverse of matrix/invrese.out 	deleted:    Povit Condensation/inverse of matrix/main.f95 	deleted:    Povit Condensation/inverse of matrix/output.txt 	deleted:    Simpson 1/3 rule/main.f95 	deleted:    Simpson 1/3 rule/output.txt 	deleted:    Simpson 1/3 rule/simpsonrule.out

--- Gauss Seidel method/main.f95
+++ Gauss Seidel method/main.f95
@@ -1,92 +1,76 @@
 program main
-
     implicit none
 
-    real , dimension(:,:) , allocatable :: matrix , dumy
-    real , dimension(:) , allocatable :: new_ans , last_ans
-    real :: u_err , err ,div , sum
-    integer :: order , i, j
+    real , allocatable :: matrix(:,:),constants(:),variable(:),guess(:),dummy(:,:)
+    real :: defined_error , error , div , sum , max_error
+    integer :: order , i , j
 
-    ! getting the number of the variable and equation and validating it
+    print *, "Enter the number of the variables are :: "
+    read *, order
 
-    print *, "Enter the number of the variable :: "
-    read *, order 
-
-    if(order<=0) then
-        stop "Invalid number of the variable"
+    if (order <= 0 ) then
+        stop 'Invalid number of variable'
     endif
 
-    ! allocate the matrix new_ans and last_ans array in the heap memory
-    allocate(dumy(order,order+1),new_ans(order+1),last_ans(order+1))
+    allocate(dummy(order,order),constants(order),variable(order),guess(order))
 
-
-    ! getting the equation the form of the matrix
-
-    print *, "Enter the equation in the form of the matrix :: "
+    print *, "Enter the value coefficients  and constants in : "
     do i = 1, order
-        do j = 1,order+1
-            if (j > order) then
-                print *,"Enter the ",i,"the equation constant :: "
-                read *, dumy(i,j)
-            else
-                print *, "Enter ",i,j,"th element :: "
-                read *, dumy(i,j)
-            endif
+        do j = 1,order
+            print *, "Enter the value of ",i,j,"th coefficient "
+            read *, dummy(i,j)
         enddo
+        print *, "Enter the ",i,"th equation's constant "
+        read *, constants(i)
     enddo
 
-    print *, "Enter the best guess for the solution of the variables :: "
-    do i = 1,order
-        print *, "Enter the value of ",i,"th element :: "
-        read *, last_ans(i)
-        new_ans(i) = 0
-    enddo
-    new_ans(order+1) = 1
-    last_ans(order+1) = 1
+    print *,order
 
-    print *, "Enter the error allowed :: "
-    read *, u_err
+    print *, "Enter the guess for the variables values : "
+    read *, (guess(i),i=1,order)
 
-    allocate(matrix(order,order+1))
+    print *, "okay"
+
+    allocate(matrix(order,order))
 
     do i = 1,order
-        div = (dumy(i,i))**(-1)
-        do j = 1,order+1
+        div = dummy(i,i)
+        if (div == 0) then
+            stop 'Enter the equation in correct order '
+        endif
+        do j = 1,order
             if (i==j) then
-                matrix(i,j) = 0
-            else
-                matrix(i,j) = -1.0*dumy(i,j)*div
+                matrix(i,i) = 0
             endif
+            matrix(i,j) = dummy(i,j)/div
         enddo
+        constants(i) = -constants(i)/div
     enddo
 
-    deallocate(dumy)
+    deallocate(dummy)
 
-    err = 1073E12
+    error = 1073E12
 
-    do while (u_err < err)
-        do i = 1,order 
+    do while(error > defined_error)
+        max_error = 0
+        do i = 1,order
             sum = 0
-            do j = 1,order+1
-                sum = sum + matrix(i,j)*last_ans(j)
+            do j = 1,order
+                sum = sum + matrix(i,j)*guess(j) 
             enddo
-            new_ans(i) = sum
-        enddo
-        err = abs(new_ans(1) - last_ans(1))
-        do i = 2,order
-            err = max(err , abs(new_ans(i) - last_ans(i)))
+            variable(i) = sum + constants(i)
+            if (abs(variable(i) - guess(i)) > max_error) max_error = abs(variable(i) -guess(i))
         enddo
         do i = 1,order
-            last_ans(i) = new_ans(i)
+            print *, variable(i)
+            guess(i) = variable(i)
         enddo
     enddo
 
-    print *, "Output::"
+    deallocate(guess,matrix,variable,constants)
 
-    do i = 1,order
-        print *, "Value of ",i,"th variable"
-        print*,new_ans(i)
-    enddo
+    print *, "The variables are with error ",error," is :: "
+    print *, (variable(i),i = 1,order)
 
     stop
 
