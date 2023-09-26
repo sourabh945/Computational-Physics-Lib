b2fc70efc891cd3d9b8b6f59275d7ad38dc604e5
Sourabh <sourabhsheokand945@gmail.com>
2023-09-26 12:42:37 +0530

    modified:   Power Method/Output.txt 	modified:   Power Method/Powermethod.out 	modified:   Power Method/main.f95

--- Power Method/main.f95
+++ Power Method/main.f95
@@ -2,7 +2,7 @@ program main
     implicit none ! declaring the variable 
 
     real , allocatable :: matrix(:,:) , vector(:) , pervious_vector(:)
-    real :: user_error , eignValue , error = 1074E12
+    real :: user_error , eigenValue , error = 1074E12
     integer :: i ,j,order , iteration 
 
     ! getting the input from the user and also verified the input and allocating the space
@@ -22,7 +22,7 @@ program main
         enddo
     enddo
 
-    print *, "Enter the guess for the eignvector :: "
+    print *, "Enter the guess for the eigenvector :: "
     read *, (pervious_vector(i),i=1,order)
 
     print *, "Enter the tolerance :: "
@@ -37,20 +37,20 @@ program main
 
     do j = 1,iteration
 
-        eignValue = 0
-        do i = 1,order     ! multiplying the matrix and also find the largest value save in eignvalue
+        eigenValue = 0
+        do i = 1,order     ! multiplying the matrix and also find the largest value save in eigenValue
 
             vector(i) = sum(matrix(i,:)*pervious_vector(:))
 
-            if (abs(vector(i)) > eignValue) eignValue = vector(i)
+            if (abs(vector(i)) > eigenValue) eigenValue = vector(i)
 
         enddo
         
         error = 0
 
-        do i = 1,order      ! Caluclation the error and making vector equal to pervious vector
+        do i = 1,order      ! Calculation the error and making vector equal to pervious vector
 
-            vector(i) = vector(i)/eignValue
+            vector(i) = vector(i)/eigenValue
 
             error = max(abs(vector(i) - pervious_vector(i)),error)
 
@@ -64,18 +64,18 @@ program main
 
 1   print *, "The eign Vector correspond to matrix in ",j,"is ",(vector(i), i= 1,order)
 
-    eignValue = 0 ! here we calculating the eignvalue by using the ratio of pervious vector and new vector
+    eigenValue = 0 ! here we calculating the eigenValue by using the ratio of pervious vector and new vector
 
     do i = 1,order
 
         vector(i) = sum(matrix(i,:)*pervious_vector(:))
-        eignValue = eignValue + vector(i)/pervious_vector(i)
+        eigenValue = eigenValue + vector(i)/pervious_vector(i)
 
     enddo
 
-    eignValue = eignValue/real(order)
+    eigenValue = eigenValue/real(order)
     
-    print *, "The eign Value is " , eignValue
+    print *, "The eign Value is " , eigenValue
 
     deallocate(vector,pervious_vector,matrix) ! deallocating the all variables
 
