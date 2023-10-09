72cf20a400996ad6388f1a80a1d64dec05d16472
Sourabh <sourabhsheokand945@gmail.com>
2023-10-09 11:34:24 +0530

    Update main.f95

---
+++ Runge Kutta Method/main.f95
@@ -0,0 +1,18 @@
+program main
+    implicit none
+
+    real , allocatable :: x(:), y(:)
+    integer :: num_of_intervals 
+    real :: x_upper , x_lower
+
+    print *, "Enter the lower limit of x: "
+    read *, x_lower
+    print *, "Enter the upper limit of x: " 
+    read *, x_upper
+    print *, "Enter the number of intervals we take "
+    read *, num_of_intervals
+
+    allocate(x(num_of_intervals),y(num_of_intervals))
+
+    print *, "Enter the value of y at lower limit of x :: "
+    
