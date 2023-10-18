c447561dfaef15f5cdcf227cb431166fb62bf14f
Sourabh <sourabhsheokand945@gmail.com>
2023-10-19 00:12:21 +0530

    modified:   Newton Forward interpolation/Single value/main.f95 	modified:   Newton Forward interpolation/continous/main.f95

--- Newton Forward interpolation/Single value/main.f95
+++ Newton Forward interpolation/Single value/main.f95
@@ -1,29 +1,29 @@
 program main 
     implicit none
-
+! declearing the variable 
     real , allocatable :: x(:),y(:),diff(:)
     real :: val , p , result
     integer :: i , j , n ,index,row
 
-    print*, "Enter the number of the given points : "
+    print*, "Enter the number of the given points : " ! getting the number of given points
     read*, n
 
-    allocate(x(n),y(n),diff(fact(n-1)))
+    allocate(x(n),y(n),diff(fact(n-1))) ! allocating the memory 
 
-    print*, "Enter the value of x and y : "
+    print*, "Enter the value of x and y : " ! getting the given point
     print*, "x      y"
     read*, (x(i),y(i),i=1,n)
 
     print*, "Enter value of x (where the value of fxn is unknown) : "
-    read*, val
+    read*, val                              ! getting the value of the x
 
-    index = cal_x0_index(val)
+    index = cal_x0_index(val) ! finding the index of x0 for given value of x
 
     if (index == 0) stop "The value of x is lower than lower limit"
 
     row = 0
 
-    do i = 1,n-index
+    do i = 1,n-index      ! calculating the forward difference table for only need points
         do j = index,n-i
             if (i==1) then
                 diff(j) = y(j+1) - y(j)
@@ -37,43 +37,43 @@ program main
     p = (val - x(index))/(x(2) - x(1))
     result = y(index)
 
-    row = 0
+    row = 0     ! here we getting the value of f(x) at that point
 
     do i = 1, n-index
         result = result + p*diff(row+index)/real(fact(i))
         p = p*(p-i)
-        row = row + n-i
+        row = row + n-i ! here this row variable help to jump from one column to next column
     enddo
 
     print*, "The value of f(X) at ",val," is :: ",result
 
     stop
 
-contains 
-    integer function fact(valu)
-        integer , intent(in) :: valu
-        integer :: l , res 
-        res = 1
-        if (valu == 1) then
-            fact = 1
-            return
-        endif
-        do l = 2, valu
-            res= res*l
-        enddo
-        fact = res
-        return 
-    end function fact
-
-    integer function cal_x0_index(value)
-        real , intent(in) :: value
-        integer :: k
-        do k = 1,n
-            if (x(k) > value) then
-                cal_x0_index = k-1
-                return 
+    contains 
+        integer function fact(valu) ! function for find the factorial 
+            integer , intent(in) :: valu
+            integer :: l , res 
+            res = 1
+            if (valu == 1) then
+                fact = 1
+                return
             endif
-        enddo
-        stop "Value of x  is over the upper limit"
-    end function cal_x0_index
+            do l = 2, valu
+                res= res*l
+            enddo
+            fact = res
+            return 
+        end function fact
+
+        integer function cal_x0_index(value) ! function for getting the index of x0 for given value of x
+            real , intent(in) :: value
+            integer :: k
+            do k = 1,n
+                if (x(k) > value) then
+                    cal_x0_index = k-1
+                    return 
+                endif
+            enddo
+            stop "Value of x  is over the upper limit"
+        end function cal_x0_index
 end program    
--- Newton Forward interpolation/continous/main.f95
+++ Newton Forward interpolation/continous/main.f95
@@ -1,32 +1,31 @@
 program main
-    implicit none
+    implicit none ! declearing the variables
 
     real , dimension(:) , allocatable :: x(:) , y(:) , diff(:)
     real :: result , p , d , val
     integer :: i , j , k , n , index , row , num_points
     logical :: f1,f2
 
-    print*, "Enter the number of the given points : "
+    print*, "Enter the number of the given points : " ! getting the number of points
     read*, n
-    if (n <= 0) stop "Invalid entry"
+    if (n <= 0) stop "Invalid entry" ! And validiting them
 
-    allocate(x(n),y(n),diff(fact(n-1)))
+    allocate(x(n),y(n),diff(fact(n-1))) ! allocatingg the memory for all arrays
+! here we are using the linear array for the forward difference table to saving the memory
 
-    print*, "Enter the value of x and y : "
+    print*, "Enter the value of x and y : " ! reading the given points and number of points generated b/w them
     print*, "x      y"
     read*, (x(i),y(i),i=1,n)
-
     print *, "Enter the number of points taking between x0 and xn :: "
     read*, num_points 
     if (num_points <= 0 ) stop "Invalid entry"
 
-    inquire(file="givenpoints.dat",exist=f1)
+    inquire(file="givenpoints.dat",exist=f1) ! opening the file for storing the value of given and calculated points
     if(f1) then
         open(1,file="givenpoints.dat",status="replace")
     else
         open(1,file="givenpoints.dat",status="new",action="write")
     endif
-
     inquire(file="calpoints.dat",exist=f2)
     if(f2) then
         open(2,file="calpoints.dat",status="replace")
@@ -35,11 +34,11 @@ program main
     endif
 
     do i=1,n
-        write(1,*)x(i),y(i)
+        write(1,*)x(i),y(i) ! writing the given points in the file 
     enddo
 
     row = 0
-    do i = 1,n-1
+    do i = 1,n-1  ! here calculating the forward difference and storing in linear array diff
         do j = 1,n-i
             if(i==1) then
                 diff(j) = y(j+1) - y(j)
@@ -47,16 +46,16 @@ program main
                 diff(row+j) = diff((row-n+i-1)+j+1) - diff((row-n+i-1)+j)
             endif
         enddo
-        row = row + n - i
+        row = row + n - i ! this variable row can give ablity to change the column of table 
     enddo
 
-    d = (x(n) - x(1))/real(num_points)
+    d = (x(n) - x(1))/real(num_points) ! getting the difference between the points we calculating 
 
-    do k = 1,num_points-1
+    do k = 1,num_points-1 ! taking a loop for calculating the points
         val = x(1) + k*d
-        index = cal_x0_index(val)
+        index = cal_x0_index(val) ! here we find the index of x0 for a given point val
         p = (val - x(index))/(x(2) - x(1))
-        result = y(index)
+        result = y(index) ! result is the value of f(x) at x
         row = 0
         do i = 1, n-index
             result = result + p*diff(row+index)/real(fact(i))
@@ -70,31 +69,31 @@ program main
 
     stop
 
-contains 
-    integer function fact(valu)
-        integer , intent(in) :: valu
-        integer :: l , res 
-        res = 1
-        if (valu == 1) then
-            fact = 1
-            return
-        endif
-        do l = 2, valu
-            res= res*l
-        enddo
-        fact = res
-        return 
-    end function fact
-
-    integer function cal_x0_index(value)
-        real , intent(in) :: value
-        integer :: k
-        do k = 1,n
-            if (x(k) > value) then
-                cal_x0_index = k-1
-                return 
+    contains 
+        integer function fact(valu) ! the function for find the factorial of the number
+            integer , intent(in) :: valu
+            integer :: l , res 
+            res = 1
+            if (valu == 1) then
+                fact = 1
+                return
             endif
-        enddo
-        stop "Value of x  is over the upper limit"
-    end function cal_x0_index
+            do l = 2, valu
+                res= res*l
+            enddo
+            fact = res
+            return 
+        end function fact
+
+        integer function cal_x0_index(value) ! this is funciton for calculating the index of 
+            real , intent(in) :: value       ! x0 for a given value of x
+            integer :: k
+            do k = 1,n
+                if (x(k) > value) then
+                    cal_x0_index = k-1
+                    return 
+                endif
+            enddo
+            stop "Value of x  is over the upper limit"
+        end function cal_x0_index
 end program
