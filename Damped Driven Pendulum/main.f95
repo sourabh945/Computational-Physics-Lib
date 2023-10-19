f1b3f6249403096e8b070e49941f034f52630c20
Sourabh <sourabhsheokand945@gmail.com>
2023-10-20 03:32:41 +0530

    modified:   main.f95

--- Damped Driven Pendulum/main.f95
+++ Damped Driven Pendulum/main.f95
@@ -1,33 +1,34 @@
 program main
     implicit none 
+    ! Refrence: https://galileoandeinstein.phys.virginia.edu/7010/CM_22a_Period_Doubling_Chaos.html
 
-    real ,dimension(:) , allocatable :: x , y , y1 
+    real ,dimension(:) , allocatable :: x , y , y1 ! declearing variables
     real :: upper_t , s_t , k  , o , r , q , h
     integer :: num_of_intervals , j
     logical :: f1,f2
 
-    print*, "Enter the value of the number of interval :: "
+    print*, "Enter the value of the number of interval :: " ! getting the number of the points
     read*, num_of_intervals
     print*, "Enter the upper limit of the time :: "
     read*, upper_t
 
-    h = upper_t/real(num_of_intervals)
+    h = upper_t/real(num_of_intervals) ! calculation the time interval 
     if(h<=0 ) stop "Invalid entry"
 
     allocate(x(num_of_intervals+1),y(num_of_intervals+1),y1(num_of_intervals+1))
     
-    x(1) = 0.0
+    x(1) = 0.0 ! defining the constants value of the equation 
     k= 1.5*1.5
     r = 0.75
 
-    print*, "Enter the value of y at t=0 ::"
+    print*, "Enter the value of y at t=0 ::" ! getting the starting point in the phase diagram 
     read*, y(1)
     print*, "Enter the value of y' at t=0 :: "
     read*, y1(1)
     print*, "Enter the value of Q :: "
     read*, q 
 
-    inquire(file="phase_diagram.dat",exist=f1)
+    inquire(file="phase_diagram.dat",exist=f1) ! opening the file for save the data
     if(f1) then
         open(1,file="phase_diagram.dat",status="replace")
     else
@@ -42,10 +43,10 @@ program main
 
     write(1,*)x(1),y(1),y1(1)
 
-    do j = 2,num_of_intervals+1
+    do j = 2,num_of_intervals+1 ! loop for calculationg the value at every time interval 
         call rkmethod(j)
-        write(1,*)x(j),y(j),y1(j)
-        if(j >= num_of_intervals*0.1) write(2,*)x(j),y(j),y1(j)
+        write(1,*)x(j),y(j),y1(j) ! writting the in the file 
+        if(j >= num_of_intervals*0.1) write(2,*)x(j),y(j),y1(j) ! deleting the first entry and save into another file
     enddo
 
     print*, "simulation is complete"
@@ -55,7 +56,7 @@ program main
     stop
 
     contains
-        real function func(val_x,val_y,val_y1)
+        real function func(val_x,val_y,val_y1) ! function of the damped driven pendulum
             real , intent(in) :: val_x,val_y,val_y1
             real :: result 
             result = q*k*cos(val_x) - k*sin(val_y) - r*val_y1
@@ -63,7 +64,7 @@ program main
             return 
         end function func
 
-        subroutine rkmethod(i)
+        subroutine rkmethod(i) ! subroutine for solving the differential equaiton 
             integer , intent(in) :: i
             real :: k1,k2,k3,k4,k11,k12,k13,k14,result
             k11 = h*y1(i-1)
