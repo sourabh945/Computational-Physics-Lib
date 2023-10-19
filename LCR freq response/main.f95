f70cc366007560ea384a4826ec8a26f60f0cbc9b
Sourabh <sourabhsheokand945@gmail.com>
2023-10-20 03:47:40 +0530

    modified:   LCR freq response/main.f95 	modified:   Runge Kutta Method/main.f95

--- LCR freq response/main.f95
+++ LCR freq response/main.f95
@@ -1,14 +1,14 @@
 program main 
 
-    implicit none
-
+    implicit none ! declaring the variables
     real :: resistance , inductance , capicatance , inv_imp , freq_lower , freq_upper , step 
     real :: resonate_freq , voltage , current , high_current , cal_resonate
     integer :: step_num , i
     real , parameter :: pi = 3.14159265
     logical :: f1
+! getting the value of resistance , inductor and capicatance from the user
 
-    print *, "Enter the value of resistance : "
+    print *, "Enter the value of resistance : " 
     read *, resistance
     print *, "Enter the value of inductance (in mH): "
     read *, inductance
@@ -17,7 +17,7 @@ program main
     print *, "Enter the value of voltage (in Volt): "
     read *,voltage
 
-
+! getting the frequency value form the user
     print *, "Enter the lower limit of frequency : "
     read *, freq_lower
     print *, "Enter the upper limit of frequency : "
@@ -25,13 +25,13 @@ program main
     print *, "Enter the step taken to from lower frequency to upper frequency : "
     read *, step_num
 
-    step = (freq_upper - freq_lower)/(real(step_num))
+    step = (freq_upper - freq_lower)/(real(step_num)) ! calculating the steps for frequency 
 
-    inductance = inductance/1000
+    inductance = inductance/1000 ! convert them into mH and mF
 
     capicatance = capicatance/1000
 
-    cal_resonate = 1/(2*pi*sqrt(inductance*capicatance))
+    cal_resonate = 1/(2*pi*sqrt(inductance*capicatance)) ! calculating the resonant from formula
 
     !check exiting file and open it and if file doesn't exit then create the files
 
@@ -44,12 +44,12 @@ program main
 
     high_current = 0.0
 
-    do i = 0,step_num
+    do i = 0,step_num ! creating the frequency response of LCR circuit
 
         inv_imp = 1/sqrt(resistance**2 + (2*pi*(freq_lower + i*step)*inductance - 1/(2*pi*(freq_lower + i*step)*capicatance))**2)
         current = inv_imp*voltage
         write(1,*)freq_lower+i*step,current
-        if(current > high_current) then
+        if(current > high_current) then ! calculating the resonant frequency from the graph
             high_current = current
             resonate_freq = freq_lower + i*step
         endif
@@ -60,10 +60,11 @@ program main
 
     print *, "Simulation complete"
     print *, ""
+! printing the output
     
     if (cal_resonate > freq_upper) then
-        print *, "Resonate frequency is higher than the upper limit of frequency"
-    else
+        print *, "Resonate frequency is higher than the upper limit of frequency" 
+    else   
         print *, "Resonate Frequency is " ,resonate_freq, "and current for ",voltage,"V is ",high_current
     endif
 
--- Runge Kutta Method/main.f95
+++ Runge Kutta Method/main.f95
@@ -1,53 +1,53 @@
 program main
-    implicit none
+    implicit none ! declearing the variables , here y1 is the first derivative of the y wrt x
 
     real , allocatable :: x(:), y(:) , y1(:)
     integer :: num_of_intervals , j
     real :: x_upper , x_lower , h
     logical :: f1
 
-    print *, "Enter the lower limit of x: "
+    print *, "Enter the lower limit of x: " ! getting the limit of x
     read *, x_lower
     print *, "Enter the upper limit of x: " 
     read *, x_upper
-    print *, "Enter the number of intervals we take "
+    print *, "Enter the number of intervals we take " ! taking the number of the interval 
     read *, num_of_intervals
     if (num_of_intervals <=0) stop "Invalid number of the intervals"
 
-    allocate(x(num_of_intervals+1),y(num_of_intervals+1),y1(num_of_intervals+1))
+    allocate(x(num_of_intervals+1),y(num_of_intervals+1),y1(num_of_intervals+1)) ! allocate the array for store the data
 
-    print *, "Enter the value of y at lower limit of x :: "
+    print *, "Enter the value of y at lower limit of x :: " ! getting the initial condition 
     read *, y(1)
     print*, "Enter the value of y' at lower limit of x :: "
     read*, y1(1)
 
-    inquire(file="points.dat",exist=f1)
+    inquire(file="points.dat",exist=f1) ! opening the file for the store the data for plot 
     if(f1) then
         open(1,file="points.dat",status="replace")
     else
         open(1,file="points.dat",status="new",action="write")
     endif
 
-    h = (x_upper - x_lower)/real(num_of_intervals)
+    h = (x_upper - x_lower)/real(num_of_intervals) ! defining the step size for  the interval 
     if (h<=0) stop "Invalid entry for upper and lower limit"
     x(1) = x_lower
 
     write(1,*)x(1),y(1),y1(1)
 
-    do j=2,num_of_intervals+1
+    do j=2,num_of_intervals+1 ! runing loop for calculate the values and store in the file 
         call rkmethod(j)
         write(1,*)x(j),y(j),y1(j)
     enddo
 
-    close(1)
+    close(1) ! printing the value of the upper limit of x
 
-    print*, "ODE is solved"
+    print*, "ODE is solved" 
     print*, "Value of y at x = ",x(num_of_intervals+1)," is  :: ",y(num_of_intervals+1)," and y' is :: ",y1(num_of_intervals+1)
     
     stop
 
     contains
-        real function func(val_x,val_y,val_y1)
+        real function func(val_x,val_y,val_y1) ! here the ODE we wanted to solve 
             real , intent(in) :: val_x,val_y,val_y1
             real :: result 
             result = exp(2*val_x)*sin(val_x) - 2*val_y + 2*val_y1
@@ -55,9 +55,9 @@ program main
             return 
         end function func
 
-        subroutine rkmethod(i)
+        subroutine rkmethod(i)   ! here we calculate the value by the rk method and this is for 2nd order equation 
             integer , intent(in) :: i
-            real :: k1,k2,k3,k4,k11,k12,k13,k14,result
+            real :: k1,k2,k3,k4,k11,k12,k13,k14,result  ! here we use 4th order rk method for 2nd order ODE
             k11 = h*y1(i-1)
             k1 = h*func(x(i-1),y(i-1),y1(i-1))
             k12 = (y1(i-1) + k1/2.0)*h
