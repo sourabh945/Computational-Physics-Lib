c53a6616f1a94c8d3e6dba12c286ddd0337bb7d7
Sourabh <sourabhsheokand945@gmail.com>
2023-10-31 23:16:38 +0530

    Revert "	new file:   Gauss Quadrature Method/2nd order/GuassQuad2.out"
    This reverts commit 99f441acf564e81ccc90e62d6eff7b7abdb7013e.

--- Gauss Quadrature Method/2nd order/main.f95
+++ Gauss Quadrature Method/2nd order/main.f95
@@ -1,74 +1,73 @@
 program main
 
-    implicit none ! declaring the variables for the program 
+    implicit none
 
     real :: upper_y,upper_x,lower_y,lower_x , integration = 0
-    real , dimension(:,:), allocatable :: optimizer ! these are those points where we do weighted sum
-    integer :: i , j, degree
+    real , dimension(:,:), allocatable :: optimiser
+    integer :: i , degree
 
     print *, "Enter the order of the polynomial  (Hint : 2 to 5):: "
     read *, degree
-    print *, "Enter the upper limit of x axis :: " ! getting the limits for the integration 
+    print *, "Enter the uppper limit of x axis :: "
     read *, upper_x
     print *, "Enter the lower limit of x axis:: "
     read *, lower_x
-    print *, "Enter the upper limit of y axis:: "
+    print *, "Enter the uppper limit of y axis:: "
     read *, upper_y
     print *, "Enter the lower limit of y axis:: "
     read *, lower_y
 
 
-    if (degree == 2) then  ! here we define the weights and points for the integration
-        allocate(optimizer(2,2))
-        optimizer(1,1) = 1
-        optimizer(1,2) = -0.5773502692
-        optimizer(2,1) = 1
-        optimizer(2,2) = 0.5773502692
+    if (degree == 2) then
+        allocate(optimiser(2,2))
+        optimiser(1,1) = 1
+        optimiser(1,2) = -0.5773502692
+        optimiser(2,1) = 1
+        optimiser(2,2) = 0.5773502692
 
     else if (degree == 3) then
-        allocate(optimizer(3,2))
-        optimizer(1,1) = 0.5555555
-        optimizer(1,2) = -0.7745967
-        optimizer(2,1) = 0.8888888
-        optimizer(2,2) = 0
-        optimizer(3,1) = 0.5555555
-        optimizer(3,2) = 0.7745967
+        allocate(optimiser(3,2))
+        optimiser(1,1) = 0.5555555
+        optimiser(1,2) = -0.7745967
+        optimiser(2,1) = 0.8888888
+        optimiser(2,2) = 0
+        optimiser(3,1) = 0.5555555
+        optimiser(3,2) = 0.7745967
 
     else if (degree == 4) then
-        allocate(optimizer(4,2))
-        optimizer(1,2) = 0.8611363116 
-        optimizer(1,1) = 0.3478548451
-        optimizer(2,2) = 0.3399810436 
-        optimizer(2,1) = 0.6521451549
-        optimizer(3,2) = -0.3399810436 
-        optimizer(3,1) = 0.6521451549
-        optimizer(4,2) = -0.8611363116 
-        optimizer(4,1) = 0.3478548451
+        allocate(optimiser(4,2))
+        optimiser(1,2) = 0.8611363116 
+        optimiser(1,1) = 0.3478548451
+        optimiser(2,2) = 0.3399810436 
+        optimiser(2,1) = 0.6521451549
+        optimiser(3,2) = -0.3399810436 
+        optimiser(3,1) = 0.6521451549
+        optimiser(4,2) = -0.8611363116 
+        optimiser(4,1) = 0.3478548451
 
     else if (degree == 5) then
-        allocate(optimizer(5,2))
-        optimizer(1,2) = 0.9061798459 
-        optimizer(1,1) = 0.2369268850
-        optimizer(2,2) = 0.5384693101 
-        optimizer(2,1) = 0.4786286705
-        optimizer(3,2) = 0.0000000000 
-        optimizer(3,1) = 0.5688888889
-        optimizer(4,2) = -0.5384693101 
-        optimizer(4,1) = 0.4786286705
-        optimizer(5,2) = -0.9061798459 
-        optimizer(5,1) = 0.2369268850
+        allocate(optimiser(5,2))
+        optimiser(1,2) = 0.9061798459 
+        optimiser(1,1) = 0.2369268850
+        optimiser(2,2) = 0.5384693101 
+        optimiser(2,1) = 0.4786286705
+        optimiser(3,2) = 0.0000000000 
+        optimiser(3,1) = 0.5688888889
+        optimiser(4,2) = -0.5384693101 
+        optimiser(4,1) = 0.4786286705
+        optimiser(5,2) = -0.9061798459 
+        optimiser(5,1) = 0.2369268850
 
     else 
         stop "Unable to do this operation for this degree of polynomial"
     endif
 
-    do i = 1,degree       ! This loops do the integration using Gauss Quadrature formula
-        do j = 1,degree
-            integration = integration + optimizer(i,1)*optimizer(j,1)*func(optimizer(i,2),optimizer(j,2))
-        enddo
+
+    do i = 1,degree
+        integration = integration + optimiser(i,1)*(GuassQuadmethod(optimiser(i,2)))
     enddo
 
-    deallocate(optimizer)
+    deallocate(optimiser)
 
     integration = (integration*(upper_y  - lower_y)*(upper_x - lower_x))/4.0
 
@@ -77,7 +76,7 @@ program main
     stop
 
     contains
-        real function func(m,t)  ! function for defining the function make them in limit 1 to -1
+        real function func(m,t)
             real , intent(in) :: t , m
             real :: output , x ,y
             x = ((upper_x - lower_x)*t + upper_x + lower_x)/2.0
@@ -86,4 +85,16 @@ program main
             func = output
             return 
         end function func
+
+        real function GuassQuadmethod(y)
+            real , intent(in) :: y
+            real :: integeration_x 
+            integer :: j
+            do j = 1,degree
+                integeration_x = integeration_x + optimiser(i,1)*func(y,optimiser(i,2))
+            enddo
+            GuassQuadmethod = integeration_x
+            return
+        end function GuassQuadmethod
+
 end program 
