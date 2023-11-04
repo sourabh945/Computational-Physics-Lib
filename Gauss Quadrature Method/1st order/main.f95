a0d2fd27fff6f490336d155718ba7bacc0f567e1
Sourabh <sourabhsheokand945@gmail.com>
2023-11-04 16:10:35 +0530

    modified:   GaussQuad1.out 	modified:   main.f95

--- Gauss Quadrature Method/1st order/main.f95
+++ Gauss Quadrature Method/1st order/main.f95
@@ -2,74 +2,74 @@ program main
 
     implicit none
 
-    ! declearing the variables need 
+    ! declaring the variables need 
 
     real :: upper,lower , integration = 0
-    real , dimension(:,:), allocatable  :: optimiser
+    real , dimension(:,:), allocatable  :: optimizer
     integer :: i , degree 
 
     ! getting the input about the upper and the lower limit and also value of n (number of points)
 
     print *, "Enter the number of points taken (Hint : 2 to 5):: "
     read *, degree
-    print *, "Enter the uppper limit :: "
+    print *, "Enter the upper limit :: "
     read *, upper
     print *, "Enter the lower limit :: "
     read *, lower
 
-    ! declearing the optimiser points value in the array
+    ! declaring the optimizer points value in the array
 
     if (degree == 2) then
-        allocate(optimiser(2,2))
-        optimiser(1,1) = 1
-        optimiser(1,2) = -0.5773502692
-        optimiser(2,1) = 1
-        optimiser(2,2) = 0.5773502692
+        allocate(optimizer(2,2))
+        optimizer(1,1) = 1
+        optimizer(1,2) = -0.5773502692
+        optimizer(2,1) = 1
+        optimizer(2,2) = 0.5773502692
 
     else if (degree == 3) then
-        allocate(optimiser(3,2))
-        optimiser(1,1) = 0.5555555
-        optimiser(1,2) = -0.7745967
-        optimiser(2,1) = 0.8888888
-        optimiser(2,2) = 0
-        optimiser(3,1) = 0.5555555
-        optimiser(3,2) = 0.7745967
+        allocate(optimizer(3,2))
+        optimizer(1,1) = 0.5555555
+        optimizer(1,2) = -0.7745967
+        optimizer(2,1) = 0.8888888
+        optimizer(2,2) = 0
+        optimizer(3,1) = 0.5555555
+        optimizer(3,2) = 0.7745967
 
     else if (degree == 4) then
-        allocate(optimiser(4,2))
-        optimiser(1,2) = 0.8611363116 
-        optimiser(1,1) = 0.3478548451
-        optimiser(2,2) = 0.3399810436 
-        optimiser(2,1) = 0.6521451549
-        optimiser(3,2) = -0.3399810436 
-        optimiser(3,1) = 0.6521451549
-        optimiser(4,2) = -0.8611363116 
-        optimiser(4,1) = 0.3478548451
+        allocate(optimizer(4,2))
+        optimizer(1,2) = 0.8611363116 
+        optimizer(1,1) = 0.3478548451
+        optimizer(2,2) = 0.3399810436 
+        optimizer(2,1) = 0.6521451549
+        optimizer(3,2) = -0.3399810436 
+        optimizer(3,1) = 0.6521451549
+        optimizer(4,2) = -0.8611363116 
+        optimizer(4,1) = 0.3478548451
 
     else if (degree == 5) then
-        allocate(optimiser(5,2))
-        optimiser(1,2) = 0.9061798459 
-        optimiser(1,1) = 0.2369268850
-        optimiser(2,2) = 0.5384693101 
-        optimiser(2,1) = 0.4786286705
-        optimiser(3,2) = 0.0000000000 
-        optimiser(3,1) = 0.5688888889
-        optimiser(4,2) = -0.5384693101 
-        optimiser(4,1) = 0.4786286705
-        optimiser(5,2) = -0.9061798459 
-        optimiser(5,1) = 0.2369268850
+        allocate(optimizer(5,2))
+        optimizer(1,2) = 0.9061798459 
+        optimizer(1,1) = 0.2369268850
+        optimizer(2,2) = 0.5384693101 
+        optimizer(2,1) = 0.4786286705
+        optimizer(3,2) = 0.0000000000 
+        optimizer(3,1) = 0.5688888889
+        optimizer(4,2) = -0.5384693101 
+        optimizer(4,1) = 0.4786286705
+        optimizer(5,2) = -0.9061798459 
+        optimizer(5,1) = 0.2369268850
 
     else 
         stop "Unable to do this operation for this degree of polynomial"
     endif
 
-    ! taking loop for all the value for calcuating the integration
+    ! taking loop for all the value for calculating the integration
 
     do i = 1,degree
-        integration = integration + optimiser(i,1)*(func(optimiser(i,2)))
+        integration = integration + optimizer(i,1)*(func(optimizer(i,2)))
     enddo
   
-    deallocate(optimiser) ! deallocate the array
+    deallocate(optimizer) ! deallocate the array
 
     integration = (integration*(upper  - lower))/2.0
 
